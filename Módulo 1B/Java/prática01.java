a19bb3122debb476b5c382ca37b793b69d474943
Willian <willian.neibertz@gmail.com>
2022-11-18 15:53:30 -0300

    Prática 01

---
+++ Módulo 1B/Java/prática01.java
@@ -0,0 +1,16 @@
+class Pratica13
+{
+   public static void main(String entrada[])
+   {
+      int idade = 20;
+      char genero = 'M';
+      double peso = 100;
+      String nome = "Willian Neibert";
+      String curso = "Analise e Desenvolvimento de Sistemas ";
+      boolean frase = false;
+
+      System.out.println("Eu sou o " + nome + ", sou do genero " + genero + ", tenho " + idade + " anos, tenho " + peso
+            + "kg de peso e curso " + curso + " e tudo o que eu disse é " + frase);
+      System.exit(0);
+   }
+}
