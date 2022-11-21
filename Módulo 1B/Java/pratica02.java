c4560a941db5abbfce3f8a4250166fe06526ac80
Willian <willian.neibertz@gmail.com>
2022-11-21 10:54:07 -0300

    Debug pratica's

--- Módulo 1B/Java/pratica02.java
+++ Módulo 1B/Java/pratica02.java
@@ -1,6 +1,5 @@
 import javax.swing.*;
-
-public class Pratica14
+class pratica14
 {
     public static void main(String entrada []) {
       //variables 
@@ -8,15 +7,15 @@ public class Pratica14
         double pot;
         String msg="";
       //input
-        n1 = Integer.parseInt(JOptionPane.showInputDialog("Digite um numero inteiro"));
-        n2 = Integer.parseInt(JOptionPane.showInputDialog("Digite outro numero inteiro"));
+        n1 = 4;
+        n2 = 6;
       //code
         div = (int)n1 / (int)n2;
         pot = Math.pow(n1, n2);
       //output
         msg = msg + "Quociente da divisao de " + n1 + " por " + n2 + " = " + div + "\n";
         msg = msg + "Potencia de " + n1 + " por " + n2 + " = " + pot;
-        JOptionPane.showMessageDialog(null, msg);
+        System.out.println(msg);
 
         System.exit(0);
  
